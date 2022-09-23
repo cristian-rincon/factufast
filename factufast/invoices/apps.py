caf5d52b9b54b684dfeab7cf203602202ad8e33c
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-09-23 12:47:20 -0500

    refactor: chmod applied to use docker into wsl

--- factufast/invoices/apps.py
+++ factufast/invoices/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InvoiceConfig(AppConfig):
+class InvoicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'invoice'
+    name = 'factufast.invoices'
